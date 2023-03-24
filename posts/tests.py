6eafcb0ad23b73a7d2cabf6ebdcca8f9d17fd3c0
mahdieh <m119.ahmadi@gmail.com>
2023-03-24 21:29:54 +0430

    add test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
+
+content_test = 'test case data'
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text = content_test)
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text , content_test)
+
+    def test_Content_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code , 200)
+
+    def test_template_correct(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response , 'home.html')
+
+    def test_template_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, content_test)
